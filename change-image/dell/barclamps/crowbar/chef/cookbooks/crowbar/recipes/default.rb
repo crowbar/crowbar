20e8571d277c660e9a73c4182282c5d357f3ceb9
Dell Openstack Crowbar Team <openstack@dell.com>
2011-08-04 05:42:50 -0500

    Have the crowbar recipie explicitly include mod_rails

--- change-image/dell/barclamps/crowbar/chef/cookbooks/crowbar/recipes/default.rb
+++ change-image/dell/barclamps/crowbar/chef/cookbooks/crowbar/recipes/default.rb
@@ -20,6 +20,7 @@
 include_recipe "apache2"
 include_recipe "apache2::mod_auth_digest"
 include_recipe "passenger_apache2"
+include_recipe "passenger_apache2::mod_rails"
 include_recipe "rails"
 
 package "curl"
