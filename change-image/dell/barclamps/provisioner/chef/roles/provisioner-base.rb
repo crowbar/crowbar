36ce8c3bf2533968491a361f32041396b6b78f7d
Dell Openstack Crowbar Team <openstack@dell.com>
2011-08-05 16:23:01 -0500

    Get rid of Ubuntu specific stuff in the provisioner

--- change-image/dell/barclamps/provisioner/chef/roles/provisioner-base.rb
+++ change-image/dell/barclamps/provisioner/chef/roles/provisioner-base.rb
@@ -2,11 +2,8 @@
 name "provisioner-base"
 description "Provisioner Base role - Apt and Networking"
 run_list(
-         "recipe[ubuntu-install::apt_repo]", 
          "recipe[utils]", 
-         "recipe[barclamp]", 
-         "recipe[apt]", 
-         "recipe[ubuntu-install::base]"
+         "recipe[barclamp]" 
 )
 default_attributes()
 override_attributes()
