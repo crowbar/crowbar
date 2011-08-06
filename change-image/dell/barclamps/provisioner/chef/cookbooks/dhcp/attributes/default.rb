80eb522dbb2e5cccd94f0075036b2493103d2b63
Dell Openstack Crowbar Team <openstack@dell.com>
2011-08-06 11:28:42 -0500

    Add required parameters for dhcpd.conf

--- change-image/dell/barclamps/provisioner/chef/cookbooks/dhcp/attributes/default.rb
+++ change-image/dell/barclamps/provisioner/chef/cookbooks/dhcp/attributes/default.rb
@@ -1,6 +1,7 @@
 
 default[:dhcp][:interfaces] = [ "eth0" ]
 default[:dhcp][:options] = [
+    "ddns-update-style ad-hoc"
     "option dhcp-client-state code 225 = unsigned integer 16",
     "option dhcp-client-state 0",
     "option dhcp-client-debug code 226 = unsigned integer 16",
