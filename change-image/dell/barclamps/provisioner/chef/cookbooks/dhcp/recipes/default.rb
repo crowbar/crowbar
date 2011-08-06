df2100a5b36a757c441751c23b28343b98c84964
Dell Openstack Crowbar Team <openstack@dell.com>
2011-08-06 00:29:12 -0500

    Fixups to dhcp for Redhat

--- change-image/dell/barclamps/provisioner/chef/cookbooks/dhcp/recipes/default.rb
+++ change-image/dell/barclamps/provisioner/chef/cookbooks/dhcp/recipes/default.rb
@@ -15,8 +15,18 @@
 
 include_recipe "utils"
 
-package "dhcp3-server"
+pkg = ""
+case node[:platform]
+when "ubunu","debian"
+  pkg = "dhcp3"
+  package "dhcp3-server"
+when "redhat","centos"
+  pkg = "dhcp"
+  package "dhcp"
+end
 
+
+directory "/etc/dhcp3"
 directory "/etc/dhcp3/groups.d"
 directory "/etc/dhcp3/subnets.d"
 directory "/etc/dhcp3/hosts.d"
@@ -37,26 +47,47 @@ file "/etc/dhcp3/hosts.d/host_list.conf" do
   mode 0644
 end
 
-template "/etc/dhcp3/dhcpd.conf" do
-  owner "root"
-  group "root"
-  mode 0644
-  source "dhcpd.conf.erb"
-  variables(:options => node[:dhcp][:options])
-end
-
 # This needs to be evaled.
 intfs = [ Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "admin").interface ]
 
-template "/etc/default/dhcp3-server" do
-  owner "root"
-  group "root"
-  mode 0644
-  source "dhcp3-server.erb"
-  variables(:interfaces => intfs)
+case node[:platform]
+when "ubuntu","debian"
+  template "/etc/dhcp3/dhcpd.conf" do
+    owner "root"
+    group "root"
+    mode 0644
+    source "dhcpd.conf.erb"
+    variables(:options => node[:dhcp][:options])
+  end
+
+  template "/etc/default/dhcp3-server" do
+    owner "root"
+    group "root"
+    mode 0644
+    source "dhcp3-server.erb"
+    variables(:interfaces => intfs)
+  end
+when "redhat","centos"
+  template "/etc/dhcpd.conf" do
+    owner "root"
+    group "root"
+    mode 0644
+    source "dhcpd.conf.erb"
+    variables(:options => node[:dhcp][:options])
+  end
+
+  template "/etc/sysconfig/dhcpd" do
+    owner "root"
+    group "root"
+    mode 0644
+    source "redhat-sysconfig-dhcpd.erb"
+    variables(:interfaces => intfs)
+  end
 end
 
+
 service "dhcp3-server" do
+  service_name "dhcpd" if node[:platform] =~ /^(redhat|centos)$/
   supports :restart => true, :status => true, :reload => true
   enabled
   action :nothing
