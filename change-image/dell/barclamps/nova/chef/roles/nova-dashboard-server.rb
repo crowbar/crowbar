18de02ba835d2287747e3f7d08914cc10656fae2
Greg Althaus <Gregory_Althaus@dell.com>
2011-09-01 14:24:24 -0500

    Remove nova,glance,and swift from the tree.

--- change-image/dell/barclamps/nova/chef/roles/nova-dashboard-server.rb
+++
@@ -1,9 +0,0 @@
-name "nova-dashboard-server"
-
-run_list(
-    "recipe[apt]",
-    "recipe[apache2]",
-    "recipe[apache2::modwsgi]",
-    "recipe[nova::dashboard]",
-    "recipe[nova::monitor]"
-)
--- change-image/dell/barclamps/nova/chef/roles/nova-mysql-server.rb
+++
@@ -1,7 +0,0 @@
-name "nova-mysql-server"
-description "mysql server setup for nova"
-
-run_list(
-  "recipe[nova::mysql]",
-  "recipe[nova::monitor]"
-)
