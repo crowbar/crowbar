2c5e57ac7b867dca1258657efd1d5a54f9c81da7
CloudEdge Solutions <rob_hirschfeld@dell.com>
2011-09-08 23:52:28 -0500

    cleanup- remove cukes

--- change-image/dell/barclamps/crowbar/crowbar_framework/config/navigation.rb
+++ change-image/dell/barclamps/crowbar/crowbar_framework/config/navigation.rb
@@ -1,7 +1,9 @@
 SimpleNavigation::Configuration.run do |navigation|  
   navigation.items do |primary|
     primary.item :dashboard, t('nav.dashboard'), root_path
-    primary.item :barclamps, t('nav.barclamps'), barclamp_index_barclamp_path
+    primary.item :barclamps, t('nav.barclamps'), barclamp_index_barclamp_path do |secondary|
+      # barclamps can add menu items here
+    end
     primary.item :proposals, t('nav.proposals'), barclamp_proposals_barclamp_path
     primary.item :roles, t('nav.roles'), barclamp_roles_barclamp_path
     primary.item :help, t('nav.help'), '/users_guide.pdf', { :link => { :target => "_blank" } }
