2013181bece3beb160bb054b02d1aa705eff76ae
Adam Spiers <aspiers@suse.com>
2012-05-18 01:04:48 +0100

    Make sure we have all the necessary SLE repositories.

--- releases/pebbles/master/extra/install-chef-suse.sh
+++ releases/pebbles/master/extra/install-chef-suse.sh
@@ -30,11 +30,16 @@ if [ $? != 0 ]; then
 fi
 
 # This is supposed to go away once the Chef dependencies are included in the
-# addon image
+# add-on image.  Note that SP1 is required for rubygem-haml at least, but
+# the new maintenance model requires SP1 repos alongside SP2 anyway.
+zypper ar -f http://dist.suse.de/install/SLP/SLES-11-SP2-LATEST/x86_64/DVD1 sp2
+zypper ar -f http://dist.suse.de/install/SLP/SLE-11-SP2-SDK-GM/x86_64/DVD1/ sdk-sp2
+zypper ar -f http://dist.suse.de/install/SLP/SLE-11-SP2-SDK-LATEST/x86_64/DVD1/ sdk-sp2
 zypper ar -f http://dist.suse.de/ibs/SUSE:/SLE-11-SP1:/GA/standard/ sp1-ga
+zypper ar -f http://dist.suse.de/ibs/SUSE:/SLE-11-SP2:/GA/standard/ sp2-ga
 zypper ar -f http://dist.suse.de/ibs/SUSE:/SLE-11-SP1:/Update/standard/ sp1-update
+zypper ar -f http://dist.suse.de/ibs/SUSE:/SLE-11-SP2:/Update/standard/ sp2-update
 zypper ar -f http://dist.suse.de/ibs/Devel:/Cloud/SLE_11_SP2/ cloud
-zypper ar -f http://dist.suse.de/install/SLP/SLE-11-SP2-SDK-GM/x86_64/DVD1/ sdk
 
 # install chef and its dependencies
 zypper --gpg-auto-import-keys in rubygem-chef-server rubygem-chef rabbitmq-server couchdb java-1_6_0-ibm rubygem-activesupport
