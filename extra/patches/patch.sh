cb7df9f1b1c97e0da6615217fe54399c2b8485d9
Victor Lowther <victor.lowther@gmail.com>
2012-05-21 12:09:06 -0500

    Allow for having multiple patchfiles for the same target.

--- extra/patches/patch.sh
+++ extra/patches/patch.sh
@@ -3,6 +3,7 @@
 # function the way we expect.
 patches=(
     'ohai/mixin/command.rb' # OHAI-330
+    'chef/mixin/command/unix.rb' # CHEF-2916
     'chef-server-ap*/app/controllers/data_item.rb' # CHEF-2005
     'chef/run_list.rb' # Always return run_lists sanely.
     'ohai-0.6.6/lib/ohai/plugins/linux/platform.rb'
@@ -15,15 +16,15 @@ for p in "${patches[@]}"; do
         echo "Cannot find file to patch for $p, will skip."
         continue
     }
-    patch="$PWD/${f##*/}.patch"
-    [[ -f $patch ]] || {
-        echo "No patch file for $f, will skip."
-        continue
-    }
-    [[ -f $f.crowbar-backup ]] && continue
-    cp "$f" "$f.crowbar-backup"
-    (cd "${f%/*}"; patch  <"$patch") && continue
-    echo "Patch $patch did not cleanly apply to $f"
-    echo "Assuming things went horribly wrong."
-    exit 1
+    for patch in "$PWD/${f##*/}"*".patch"; do
+        [[ -f $patch ]] || {
+            echo "No patch file for $f, will skip."
+            continue 2
+        }
+        [[ -f $f.crowbar-backup ]] && continue 2
+        cp "$f" "$f.crowbar-backup"
+        (cd "${f%/*}"; patch <"$patch") && continue 2
+        mv "$f.crowbar-backup" "$f"
+        echo "Patch $patch did not cleanly apply to $f, skipping it."
+    done
 done
