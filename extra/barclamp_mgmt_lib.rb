99c8df53edc74a8a168809b24a58c5d63cd76dd5
Christoph Thiel <ct@kki.org>
2012-08-25 23:30:34 +0200

    use correct path for reading description for barclamps (bnc#776916)

--- extra/barclamp_mgmt_lib.rb
+++ extra/barclamp_mgmt_lib.rb
@@ -83,7 +83,7 @@ def catalog(bc_path)
     description = bc['barclamp']['description']
     if description.nil?
       debug "Trying to find description"
-      [ File.join(bc_path, '..', name, 'chef', 'data_bags', 'crowbar', "bc-template-#{name}.json"), \
+      [ File.join(bc_path, '..', '..', 'chef', 'data_bags', 'crowbar', "bc-template-#{name}.json"), \
         File.join(bc_path, '..', "barclamp-#{name}", 'chef', 'data_bags', 'crowbar', "bc-template-#{name}.json")].each do |f|
         next unless File.exist? f
         s = JSON::load File.open(f, 'r')
