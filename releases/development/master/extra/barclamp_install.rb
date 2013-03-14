crowbar/crowbar

commit e3efe5ba6394bfe642fe5711a10e5b8163347394

Make sure gemfile_plugin is actually synced to disk
With the old code it could happen that the contents of the file weren't
present at the time when the railties:install:migrations rake task was
executed. (Which was leading to installation failures later on)

releases/development/master/extra/barclamp_install.rb
@@ -169,8 +169,12 @@ class BarclampFS
         FileUtils.mkdir_p(gemfile_dir)
         routes_plugin  = File.join(routes_dir,  "barclamp-#{@name}-engine.routes")
         gemfile_plugin = File.join(gemfile_dir, "barclamp-#{@name}.gemfile")
-        File.new(routes_plugin,  'w').puts("mount #{engine_class}, :at => '#{@name}'") unless File.exists?(routes_plugin)
-        File.new(gemfile_plugin, 'w').puts("gem '#{gem_name}', :path => File.join(crowbar_path, '..', 'barclamps', '#{@name}', '#{engine_path}')") unless File.exists?(gemfile_plugin)
+        File.new(routes_plugin,  'w') do |f|
+          f.puts("mount #{engine_class}, :at => '#{@name}'")
+        end unless File.exists?(routes_plugin)
+        File.open(gemfile_plugin, 'w') do |f|
+          f.puts("gem '#{gem_name}', :path => File.join(crowbar_path, '..', 'barclamps', '#{@name}', '#{engine_path}')")
+        end unless File.exists?(gemfile_plugin)
       when 'bin'
         debug("Installing commands for #{@name}")
         FileUtils.mkdir_p(File.join(target,'bin'))

commit 4ac1021ae8d23b7bd75dfe4448f354e5cc0defaf

File::new() does not take block; use File::open() instead
Hotfix for breakage introduced by e3efe5ba :(

releases/development/master/extra/barclamp_install.rb
@@ -169,7 +169,7 @@ class BarclampFS
         FileUtils.mkdir_p(gemfile_dir)
         routes_plugin  = File.join(routes_dir,  "barclamp-#{@name}-engine.routes")
         gemfile_plugin = File.join(gemfile_dir, "barclamp-#{@name}.gemfile")
-        File.new(routes_plugin,  'w') do |f|
+        File.open(routes_plugin,  'w') do |f|
           f.puts("mount #{engine_class}, :at => '#{@name}'")
         end unless File.exists?(routes_plugin)
         File.open(gemfile_plugin, 'w') do |f|