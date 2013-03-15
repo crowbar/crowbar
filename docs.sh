e510c261d93d5ed4edd46bd2ab0944d627a38aaa
Rob Hirschfeld <rob_hirschfeld@dell.com>
2013-03-14 18:20:32 -0700

    changes based on Victor's suggestions

--- docs.sh
+++ docs.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Start rails
-cd /tmp/crowbar-dev-test/opt/dell/crowbar_framework
-bundle exec script/rails s Puma >> log/test.out 2>> log/test.err &
-sleep 10
-cd ../doc
-
 # Run Book Gen
-for book in [gettingstarted releasenotes userguide barclamps deployguide devguide licenses]; do
-  curl "http://127.0.0.1:3000/docs/topic/framework/$book?expand=true" -o "$book.html"
+for book in gettingstarted releasenotes userguide barclamps deployguide devguide licenses; do
+  curl "http://127.0.0.1:3000/docs/topic/framework/$book?expand=true" -o "/tmp/crowbar-dev-test/opt/dell/doc/$book.html" -#
+  echo "exported /tmp/crowbar-dev-test/opt/dell/doc/$book.html"
 done
 
-# Stop rails
-kill `ps -ef | grep Puma | grep ruby | awk '{ print $2 }'`
 
