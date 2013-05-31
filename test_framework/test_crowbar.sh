4a8c83aec6417ca79af70f996963852710071b64
Victor Lowther <victor.lowther@gmail.com>
2013-05-31 09:36:04 -0500

    Make smoketest cleanup work even when there is no smoketest_dir.

--- test_framework/test_crowbar.sh
+++ test_framework/test_crowbar.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 
 # This script expects to be able to run certian commands as root.
 # Either run it as a user who can sudo to root, or give the user
@@ -21,7 +21,7 @@
 
 # We use bash4 specific functionality (hash tables), and cgroups to
 # make sure we clean up everything when we exit a test run.
-# You will need a fairly recent Linux distro to run this test -- 
+# You will need a fairly recent Linux distro to run this test --
 # RHEL/CentOS 5 will not work without some significant rework.
 
 readonly currdir="$PWD"
@@ -30,7 +30,7 @@ readonly currdir="$PWD"
 [[ $CROWBAR_DIR ]] || CROWBAR_DIR="${0%/*}/.."
 [[ $CROWBAR_DIR = /* ]] || CROWBAR_DIR="$currdir/$CROWBAR_DIR"
 [[ -f $CROWBAR_DIR/build_crowbar.sh && -d $CROWBAR_DIR/.git ]] || \
-    die "$CROWBAR_DIR is not a git checkout of Crowbar!" 
+    die "$CROWBAR_DIR is not a git checkout of Crowbar!"
 export CROWBAR_DIR
 
 . "$CROWBAR_DIR/build_lib.sh"
@@ -47,7 +47,7 @@ Arguments:
    develop-mode: Causes the framework to pause instead of automatically
                  exiting whenever a test fails, a timeout is exceeded, or
                  the test would otherwise exit sucessfully.
-   pause: Causes the framework unconditionally pause after all the tests 
+   pause: Causes the framework unconditionally pause after all the tests
           have finished.
    pause-after-admin: Causes the framework to pause after the admin node
                       has been deployed.
@@ -63,17 +63,17 @@ Arguments:
 
 VM Specifications:
    By default, the smoketest framework will try and deploy a 5 node cluster.
-   
+
    The admin node will have 4 CPUs, 4 gigs of RAM, 1 hard drive with
    15 gigs of disk space, and 2x E1000 nics.
 
    The compute nodes will have 2 CPUs, 2 gigs of RAM, i hard drive with
-   10 gigs of space for the OS, 2 hard drives with a gig of space, 
+   10 gigs of space for the OS, 2 hard drives with a gig of space,
    and 2x E1000 nics.
 
 Networking:
    The smoketest framework creates 2 bridges, and attaches one nic from each
-   VM to each bridge.  The primary bridge is given an IP address of 
+   VM to each bridge.  The primary bridge is given an IP address of
    192.168.124.1, and the framework assumes that it can contact the admin node
    at 192.168.124.10.
 
@@ -85,8 +85,8 @@ Logging and Display:
    By default, the smoketest framework will arrange for all the VMs to log
    to a serial capture, which is saved in $CROWBAR_DIR/testing.  This is also
    where error output from the scripts are captured, the VM images and state
-   information is saved, etc.  The framework also runs a screen session to 
-   display various important logs, provide a shell running in the same 
+   information is saved, etc.  The framework also runs a screen session to
+   display various important logs, provide a shell running in the same
    environment as the framework, and (if the DISPLAY variable is not present
    in the environment) to provide access to the consoles of the vms.
    If DISPLAY is set, the VMs will run using an SDL display.
@@ -99,18 +99,19 @@ EOF
 
 case $1 in
     cleanup) shift;
-	    CGROUP_DIR=$(sudo "$SMOKETEST_DIR/make_cgroups.sh" $$ crowbar-test) || \
-		die "Could not mount cgroup filesystem!"
-	    
-	    for smoketest_dir in "$CROWBAR_DIR/testing/"*; do
-		[[ -d $smoketest_dir && $smoketest_dir != */cli ]] || continue
-		smoketest_cleanup
-	    done
-	    for l in "$SMOKETEST_LOCK" "$SMOKETEST_KVM_LOCK" \
-		"$SMOKETEST_CLEANUP_LOCK"; do
-		rm -f "$l"
-	    done;;
+            CGROUP_DIR=$(sudo "$SMOKETEST_DIR/make_cgroups.sh" $$ crowbar-test) || \
+                die "Could not mount cgroup filesystem!"
+            for smoketest_dir in "$CROWBAR_DIR/testing/"*; do
+                [[ -d $smoketest_dir && $smoketest_dir != */cli ]] || continue
+                smoketest_cleanup
+            done
+            unset smoketest_dir
+            smoketest_cleanup
+            for l in "$SMOKETEST_LOCK" "$SMOKETEST_KVM_LOCK" \
+                "$SMOKETEST_CLEANUP_LOCK"; do
+                rm -f "$l"
+            done;;
     ''|help) do_help;;
-    run-test) shift; run_test "$@" ;; 
+    run-test) shift; run_test "$@" ;;
     *) run_test "$@" ;;
 esac
