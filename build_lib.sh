c7cd5d2718372d52ea69e6895cd11faf3c4e15cc
John H Terpstra <John_Terpstra@Dell.Com>
2013-11-13 11:32:04 -0600

    openSUSE ISO build updates.

--- build_lib.sh
+++ build_lib.sh
@@ -473,7 +473,7 @@ make_chroot() {
 cache_add() {
     # $1 = file to add.
     # $2 = location to store it in the cache
-    cp "$1" "$2" || \
+    sudo cp "$1" "$2" || \
         die "Cannot save $1 in $2!"
     if [[ $CURRENT_CACHE_BRANCH ]]; then
         CACHE_NEEDS_COMMIT=true
@@ -486,7 +486,7 @@ cache_rm() {
         CACHE_NEEDS_COMMIT=true
         in_cache git rm -f "${1#${CACHE_DIR}/}"
     fi
-    rm -f "$1"
+    sudo rm -f "$1"
 }
 
 make_barclamp_pkg_metadata() {
@@ -528,6 +528,18 @@ update_barclamp_src_pkg_cache() {
     done
 }
 
+# This function manages the target pkg directory structure
+copy_bc_packages() {
+    is_pkg "$CHROOT/$CHROOT_PKGDIR/$pkg" || continue
+    [[ ${pkgs["$pkg"]} = true ]] && continue
+    if [[ ${pkg%/*} != '.' ]]; then
+        [[ -d $bc_cache/${pkg%/*} ]] || sudo mkdir -p "$bc_cache/${pkg%/*}"
+        [[ -f $bc_cache/${pkg##*/} ]] && cache_rm "$bc_cache/${pkg##*/}"
+    fi
+    cache_add "$CHROOT/$CHROOT_PKGDIR/$pkg" "$bc_cache/${pkg//%3a/:}"
+}
+
+
 # Update the package cache for a barclamp.
 update_barclamp_pkg_cache() {
     # $1 = barclamp we are working with
@@ -552,13 +564,7 @@ update_barclamp_pkg_cache() {
         die "Could not fetch packages required by barclamp $1"
     mkdir -p "$bc_cache"
     while read pkg; do
-        is_pkg "$CHROOT/$CHROOT_PKGDIR/$pkg" || continue
-        [[ ${pkgs["$pkg"]} = true ]] && continue
-        if [[ ${pkg%/*} != '.' ]]; then
-            [[ -d $bc_cache/${pkg%/*} ]] || mkdir -p "$bc_cache/${pkg%/*}"
-            [[ -f $bc_cache/${pkg##*/} ]] && cache_rm "$bc_cache/${pkg##*/}"
-        fi
-        cache_add "$CHROOT/$CHROOT_PKGDIR/$pkg" "$bc_cache/${pkg//%3a/:}"
+        copy_bc_packages
     done < <(cd "$CHROOT/$CHROOT_PKGDIR"; find -type f)
     local force_update=true
     update_barclamp_src_pkg_cache "$1"
