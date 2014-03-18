8cd0e3fa3f1109a29f2a24162b7339428935a0ab
Valerii Zhelezniakov <vzhelezniakov@mirantis.com>
2014-03-18 17:00:49 +0200

    Implement dir2pi functionality into pip-bundle.py module

--- pip-bundler.py
+++ pip-bundler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 import pip
+import pkg_resources
 import sys, os, tarfile, zipfile, shutil, urllib2, getopt
 from distutils.version import StrictVersion
 from pip.util import splitext
@@ -89,9 +90,93 @@ class Bundler:
                 print(error)
                 exit(1)
 
+
+class Dir2Pi:
+    def __init__(self, cache_directory):
+        self.cache_directory = cache_directory
+
+    def egg_to_package(self, file):
+        """ Extracts the package name from an egg.
+            egg_to_package("PyYAML-3.10-py2.7-macosx-10.7-x86_64.egg")
+            ('PyYAML', '3.10-py2.7-macosx-10.7-x86_64.egg')
+            egg_to_package("python_ldap-2.3.9-py2.7-macosx-10.3-fat.egg")
+            ('python-ldap', '2.3.9-py2.7-macosx-10.3-fat.egg')
+        """
+        dist = pkg_resources.Distribution.from_location(None, file)
+        name = dist.project_name
+        return name, file[len(name)+1:]
+
+    def file_to_package(self, file, basedir=None):
+        """ Returns the package name for a given file.
+            file_to_package("foo-1.2.3_rc1.tar.gz")
+            ('foo', '1.2.3-rc1.tar.gz')
+            file_to_package("foo-bar-1.2.tgz")
+            ('foo-bar', '1.2.tgz')
+        """
+        if os.path.splitext(file)[1].lower() == ".egg":
+            return self.egg_to_package(file)
+        split = file.rsplit("-", 1)
+        if len(split) != 2:
+            msg = "unexpected file name: %s " % file
+            msg += "(not in 'pkg-name-version.xxx' format"
+            if basedir:
+                msg += "; found in directory: %s" % basedir
+            msg += ")"
+            raise ValueError(msg)
+        return split[0], pkg_resources.safe_name(split[1])
+
+    def create_dir_simple(self):
+        """
+            Creates the directory self.cache_directory/simple/ and populates
+            it with the directory structure required to use with
+            pip's --index-url.
+
+            Assumes that self.cache_directory contains a bunch of archives
+            named
+            'package-name-version.ext' (ex 'foo-2.1.tar.gz' or
+            'foo-bar-1.3rc1.bz2').
+
+            For example:
+
+                $ ls packages/
+                foo-1.2.tar.gz
+                $ dir2pi packages/
+                $ find packages/
+                packages/
+                packages/foo-1.2.tar.gz
+                packages/simple/
+                packages/simple/foo/
+                packages/simple/foo/foo-1.2.tar.gz
+        """
+        pkgdir = self.cache_directory
+        if not os.path.isdir(pkgdir):
+            raise ValueError("no such directory: %r" % (pkgdir, ))
+        pkgdirpath = lambda *x: os.path.join(pkgdir, *x)
+
+        shutil.rmtree(pkgdirpath("simple"), ignore_errors=True)
+        os.mkdir(pkgdirpath("simple"))
+
+        for file in os.listdir(pkgdir):
+            pkg_filepath = os.path.join(pkgdir, file)
+            if not os.path.isfile(pkg_filepath):
+                continue
+            pkg_basename = os.path.basename(file)
+            if pkg_basename.startswith("."):
+                continue
+            pkg_name, pkg_rest = self.file_to_package(pkg_basename, pkgdir)
+            pkg_dir = pkgdirpath("simple", pkg_name)
+            if not os.path.exists(pkg_dir):
+                os.mkdir(pkg_dir)
+            pkg_new_basename = "-".join([pkg_name, pkg_rest])
+            symlink_target = os.path.join(pkg_dir, pkg_new_basename)
+            symlink_source = os.path.join("../../", pkg_basename)
+            os.symlink(symlink_source, symlink_target)
+
+
 def print_help():
     print 'pip-bundler.py <options> -c <cache directory> requires'
     print '\t-f\tfetch packages if not cached'
+
 
 def main(argv):
     cache_directory = None
@@ -115,8 +200,10 @@ def main(argv):
         exit(1)
 
     bundler = Bundler(str(cache_directory))
-
     bundler.resolve(args, update=cache_update)
+
+    dir2pi = Dir2Pi(str(cache_directory))
+    dir2pi.create_dir_simple()
 
 
 if __name__ == "__main__":
