5710511b9cd85ae3a49682b25058f382a9dd84fe
Thomas Boerger <tboerger@suse.de>
2014-05-26 11:44:01 +0200

    Replaced copyright notice

--- releases/stoney/master/extra/barclamp_extract_locale.rb
+++ releases/stoney/master/extra/barclamp_extract_locale.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/ruby
-# Copyright 2012, SUSE Linux Products GmbH
+#
+# Copyright 2011-2013, Dell
+# Copyright 2013-2014, SUSE LINUX Products GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 require 'getoptlong'
 require 'yaml'
 
