5ecc177821029966edc792e93768af1e92dd9ffc
Vincent Untz <vuntz@suse.com>
2015-06-29 17:47:59 +0200

    Remove ./dev tool, build scripts, packaging scripts, test framework
    All these bits are deeply connected to each other, and since we don't
    use the ./dev tool anymore, let's just drop everything.
    
    The build script was used to create an ISO with everything, but we want
    to use packages instead.
    
    The packaging should be done downstream.
    
    The test framework is now moved to mkcloud and other similar scripts.

--- test_framework/test_admin_deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh root@192.168.124.10 -- /bin/bash -c 'test -f /var/log/install.log && ! pidof script &>/dev/null && ! grep -q "Admin node deployed" /var/log/install.log' 2>/dev/null 
