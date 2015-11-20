bf58f27d4c2feaeca480adca104bdca5839db985
Michal Jura <mjura@suse.com>
2015-11-20 08:37:02 +0100

    Update SLES12 SP1 symlinks for SMT-mirrored repos

--- scripts/install-chef-suse.sh
+++ scripts/install-chef-suse.sh
@@ -460,40 +460,40 @@ for arch in $supported_arches; do
   smt_dir=/srv/www/htdocs/repo/SUSE/Updates/SLE-SERVER/12/$arch/update
   test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
 
-  cloud_dir=/srv/tftpboot/suse-12.1/repos/$arch/SLES12-SP1-Pool
+  cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SLES12-SP1-Pool
   smt_dir=/srv/www/htdocs/repo/SUSE/Products/SLE-SERVER/12-SP1/$arch/product
   test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
 
-  cloud_dir=/srv/tftpboot/suse-12.1/repos/$arch/SLES12-SP1-Updates
+  cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SLES12-SP1-Updates
   smt_dir=/srv/www/htdocs/repo/SUSE/Updates/SLE-SERVER/12-SP1/$arch/update
   test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
 
-  cloud_dir=/srv/tftpboot/suse-12.1/repos/$arch/SUSE-OpenStack-Cloud-6-Pool
+  cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SUSE-OpenStack-Cloud-6-Pool
   smt_dir=/srv/www/htdocs/repo/SUSE/Products/OpenStack-Cloud/6/$arch/product
   test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
 
-  cloud_dir=/srv/tftpboot/suse-12.1/repos/$arch/SUSE-OpenStack-Cloud-6-Updates
+  cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SUSE-OpenStack-Cloud-6-Updates
   smt_dir=/srv/www/htdocs/repo/SUSE/Updates/OpenStack-Cloud/6/$arch/update
   test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
 
   # SES is x86_64 only
   if [ $arch == x86_64 ]; then
-    cloud_dir=/srv/tftpboot/suse-12.0/repos/$arch/SUSE-Enterprise-Storage-2-Pool
-    smt_dir=/srv/www/htdocs/repo/SUSE/Products/Storage/2/$arch/product
+    cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SUSE-Enterprise-Storage-2.1-Pool
+    smt_dir=/srv/www/htdocs/repo/SUSE/Products/Storage/2.1/$arch/product
     test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
 
-    cloud_dir=/srv/tftpboot/suse-12.0/repos/$arch/SUSE-Enterprise-Storage-2-Updates
-     smt_dir=/srv/www/htdocs/repo/SUSE/Updates/Storage/2/$arch/update
+    cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SUSE-Enterprise-Storage-2-Updates
+     smt_dir=/srv/www/htdocs/repo/SUSE/Updates/Storage/2.1/$arch/update
     test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
   fi
 
   # HA is s390x/x86_64 only
   if [ $arch == x86_64 -o $arch == s390x ]; then
-    cloud_dir=/srv/tftpboot/suse-12.1/repos/$arch/SLE12-SP1-HA-Pool
+    cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SLE12-SP1-HA-Pool
     smt_dir=/srv/www/htdocs/repo/SUSE/Products/SLE-HA/12-SP1/$arch/product
     test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
 
-    cloud_dir=/srv/tftpboot/suse-12.1/repos/$arch/SLE12-SP1-HA-Updates
+    cloud_dir=/srv/tftpboot/suse-12.1/$arch/repos/SLE12-SP1-HA-Updates
     smt_dir=/srv/www/htdocs/repo/SUSE/Updates/SLE-HA/12-SP1/$arch/update
     test ! -e $cloud_dir -a -d $smt_dir && ln -s $smt_dir $cloud_dir
   fi
